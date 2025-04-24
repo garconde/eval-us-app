8ad41aa955c57e2545b9690eca7f38b184dfb158
garconde <david.jose97@hotmail.com>
2025-04-24 03:31:07 -0500

    Actualizar el listado al buscar

--- src/componentes/barra.js
+++ src/componentes/barra.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/fontawesome.min.css";
 import "../css/fontawesome.css";
 import "../css/sb-admin-2.min.css";
@@ -8,101 +8,64 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Nuevo from "./nuevo";
 import Info from "./estaticos/info";
 
-export default function Barra({actualizarListado, actualizarNoti}) {
-    //const [listaaux, setlistaaux] = useState([]);
+export default function Barra({ listaOriginal, actualizarListado, actualizarNoti }) {
+    const [busqueda, setBusqueda] = useState("");
 
-    function actualizarListaAux(elementos){
+    const handleChange = (e) => {
+        const valor = e.target.value;
+        setBusqueda(valor);
+
+        if (!valor.trim()) {
+            actualizarListado(listaOriginal);
+            return;
+        }
+
+        const listaFiltrada = listaOriginal.filter(item =>
+            item.nombre.toLowerCase().includes(valor.toLowerCase())
+        );
+        actualizarListado(listaFiltrada);
+    };
+
+    function actualizarListaAux(elementos) {
         actualizarListado(elementos);
     }
 
-    function actualizarNotiAux(newState, newMessage, newType){
+    function actualizarNotiAux(newState, newMessage, newType) {
         actualizarNoti(newState, newMessage, newType);
     }
 
     return (
         <div>
-            {/* Topbar */}
             <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-
-
-                {/* Topbar Search */}
                 <form className="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
                     <div className="input-group">
                         <input
                             type="text"
                             className="form-control bg-light border-0 small"
                             placeholder="Buscar software..."
-                            aria-label="Search"
+                            aria-label="Buscar"
                             aria-describedby="basic-addon2"
+                            value={busqueda}
+                            onChange={handleChange}
                         />
                         <div className="input-group-append">
                             <button className="btn btn-primary" type="button">
-                                <FontAwesomeIcon className="btn btn-primary" type="button" icon={faSearch} />                               
-                                 </button>
+                                <FontAwesomeIcon icon={faSearch} />
+                            </button>
                         </div>
                     </div>
                 </form>
-                {/* Topbar Navbar */}
-
 
                 <ul className="navbar-nav ml-auto">
-
-                    {/* Nav Item - Search Dropdown (Visible Only XS) */}
-                    <li className="nav-item dropdown no-arrow d-sm-none">
-                        <a
-                            className="nav-link dropdown-toggle"
-                            href="#"
-                            id="searchDropdown"
-                            role="button"
-                            data-toggle="dropdown"
-                            aria-haspopup="true"
-                            aria-expanded="false"
-                        >
-                            <i className="fas fa-search fa-fw" />
-                        </a>
-                        {/* Dropdown - Messages */}
-                        <div
-                            className="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                            aria-labelledby="searchDropdown"
-                        >
-                            <form className="form-inline mr-auto w-100 navbar-search">
-                                <div className="input-group">
-                                    <input
-                                        type="text"
-                                        className="form-control bg-light border-0 small"
-                                        placeholder="Search for..."
-                                        aria-label="Search"
-                                        aria-describedby="basic-addon2"
-                                    />
-                                    <div className="input-group-append">
-                                        <button className="btn btn-primary" type="button">
-                                            <i className="fas fa-search fa-sm" />
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </li>
-
-
-                    {/* Nav Item - Nuevo */}
                     <li className="nav-item">
-                        <Nuevo 
-                            actualizarList = {actualizarListaAux} 
-                            actualizarNotif = {actualizarNotiAux} />
+                        <Nuevo actualizarList={actualizarListaAux} actualizarNotif={actualizarNotiAux} />
                     </li>
-
-
                     <div className="topbar-divider d-none d-sm-block" />
-
-                    {/* Nav Item - Information */}
                     <li className="nav-item">
                         <Info />
                     </li>
                 </ul>
             </nav>
-            {/* End of Topbar */}
         </div>
     );
 }
